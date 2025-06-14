8c418d719a174cace7246704a03e0f4330f1603e
0lolxd0xd0xd0xd0 <may14th@outlook.com>
2025-06-14 20:33:41 +0100

    Update tank.lua

--- tank.lua
+++ tank.lua
@@ -3,7 +3,7 @@ local Settings = {
 		Enabled = true; -- Whether or not the reach is enabled or not
 		Distance = 4; -- Distance around the tools handle
 
-		LimbSelection = {["Left Arm"] = true, ["Left Leg"] = true, ["Right Arm"] = false, ["Right Leg"] = true, ["Torso"] = false, ["Head"] = false}; -- Limbs that will be brung to your sword.
+		LimbSelection = {["Left Arm"] = true, ["Left Leg"] = true, ["Right Arm"] = false, ["Right Leg"] = true, ["Torso"] = false, ["Head"] = true}; -- Limbs that will be brung to your sword.
 
 		HitRate = 1; -- Rate at which the limbs will be hit.
 		LungeOnly = true; -- Whether or not the reach will be active only on lunge
